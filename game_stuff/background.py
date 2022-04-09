c0b2eadc9915c0687d3a1eb33a4f97402b3b5a3a
John Moore <johnemw@ad.unc.edu>
2022-04-09 02:51:07 -0400

    NEW

--- game_stuff/background.py
+++ game_stuff/background.py
@@ -1,6 +1,11 @@
+import pygame
+
+
 class Background():
     x: int
     y: int
+    image: pygame.surface.Surface
+
     def __init__(self, x = 0, y = 0):
         self.image = self.image.load("game_stuff/assets/magma.png")
     
