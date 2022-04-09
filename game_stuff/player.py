0c34402d045526e02a11741fdd325828a3c88853
Benjamin Eldridge <beldridge@unc.edu>
2022-04-09 02:24:21 -0400

    TarHeelMan has landed

--- game_stuff/player.py
+++ game_stuff/player.py
@@ -6,7 +6,7 @@ class Player(pygame.sprite.Sprite):
      
     def __init__(self, x: int, y: int, width: int, height: int):
         super().__init__()
-        self.image = pygame.image.load("game_stuff/assets/ninja.bmp")
+        self.image = pygame.image.load("game_stuff/assets/TARHEELMAN.png")
         self.rect = pygame.rect.Rect(x, y, width, height)
         self.image.set_colorkey((50,50,40))
 
@@ -14,9 +14,9 @@ class Player(pygame.sprite.Sprite):
         # self.movey += 3.2
         # if self.rect.y> >
 
-    def update(self):
-        # self.rect.x = self.rect.x + self.movex
-    # def jump(self):
+    # def update(self):
+    #     # self.rect.x = self.rect.x + self.movex
+    #     # def jump(self):
 
     def draw(self, screen) -> None:
         screen.blit(self.image, self.rect)
