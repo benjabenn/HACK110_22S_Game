22e1c8571b6eac3bbc7d24780bb447bc08282e17
Benjamin Eldridge <beldridge@unc.edu>
2022-04-09 01:34:55 -0400

    Game Loop Created

--- game_stuff/game.py
+++ game_stuff/game.py
@@ -1,1 +1,19 @@
 """The start of bad-megaman"""
+
+import pygame
+
+pygame.init()
+
+display = pygame.display.set_mode((600, 600))
+
+while True:
+
+    display.fill((123, 175, 212))
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    
+
+    pygame.display.update()
