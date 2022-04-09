23c9ffc4947867b726de414741e0909f4f86b8f7
John Moore <johnemw@ad.unc.edu>
2022-04-09 02:48:32 -0400

    back2

--- game_stuff/background.py
+++ game_stuff/background.py
@@ -1,8 +1,10 @@
 class Background():
-    def __init__(self):
+    x: int
+    y: int
+    def __init__(self, x = 0, y = 0):
         self.image = self.image.load("game_stuff/assets/magma.png")
     
-    def draw(self, display):
-        display.blit(self.image, (0, 0))
+    def draw(self, display, x, y):
+        display.blit(self.image, (x, y))
 
         
