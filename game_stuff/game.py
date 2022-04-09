13832252419866fee3cd9ee0bdf300fd0caf8cc9
Benjamin Eldridge <beldridge@unc.edu>
2022-04-09 04:37:07 -0400

    Player Movement Complete

--- game_stuff/game.py
+++ game_stuff/game.py
@@ -10,23 +10,27 @@ from player import Player
 pygame.init()
 
 display = pygame.display.set_mode((600, 600))
+player = Player(200, 200, 128, 128)
 
 while True:
 
     display.fill((123, 175, 212))
+
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        # elif i.type == pygame.KEYDOWN:
-        #     if i.key == pygame.K_LEFT:
-        #         play.rect.move_ip(-10, 0)
-        #     elif i.key == pygame.K_RIGHT:
-        #         play.rect.move_ip(10, 0)
-        #     elif i.key == pygame.K_UP:
-        #         play.rect.move_ip(0, 10)
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT:
+                player.rect.move_ip(-10, 0)
+            elif i.key == pygame.K_RIGHT:
+                player.rect.move_ip(10, 0)
+            elif i.key == pygame.K_UP:
+                player.rect.move_ip(0, -10)
+            elif i.key == pygame.K_DOWN:
+                player.rect.move_ip(0, 10)
     
-    player: Player = Player(200, 200, 128, 128)
+
     background: Background = Background()
 
     background.draw(display)
--- game_stuff/player.py
+++ game_stuff/player.py
@@ -1,15 +1,15 @@
 """PLAYER STUFF"""
+
 import pygame
 from pygame.locals import RLEACCEL
 
 class Player(pygame.sprite.Sprite):
      
-    def __init__(self, x: int, y: int, width: int, height: int):
+    def __init__(self, x: int, y: int, width: int, height: int, speed: int = 20):
         super().__init__()
         self.image = pygame.image.load("game_stuff/assets/TARHEELMAN.png")
         self.rect = pygame.rect.Rect(x, y, width, height)
         self.image.set_colorkey((50,50,40))
-    
 
     def draw(self, screen) -> None:
         screen.blit(self.image, self.rect)
